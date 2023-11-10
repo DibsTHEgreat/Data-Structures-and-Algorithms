492609b84386ff09553e71d0507b2795bef07b9a
patd3720 <divya.pateliya@uleth.ca>
2023-11-09 17:07:08 -0700

    Adding Data Structure Graph

---
+++ Graphs/graphs.py
@@ -0,0 +1,7 @@
+# In this file I will be creating a class for the data structure known as
+# graphs using adjacency lists.
+class Graph:
+    # Constructor for the graph class, all I'm doing is creating an empty
+    # dictionary.
+    def __init__(self):
+        self.adj_list = {}
